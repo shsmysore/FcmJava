09f3db8a3cbce16c8cbdef6a2f68e599fb893aaf
Shree Harsha S <shsmysore@gmail.com>
2021-09-13 15:49:43 +0530

    Rename package

---
+++ FcmJava/fcmjava-core/src/main/java/com/github/shsmysore/fcmjava/requests/data/DataUnicastMessage.java
@@ -0,0 +1,41 @@
+// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.github.shsmysore.fcmjava.requests.data;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.shsmysore.fcmjava.requests.notification.NotificationPayload;
+import com.github.shsmysore.fcmjava.model.options.FcmMessageOptions;
+import com.github.shsmysore.fcmjava.requests.FcmUnicastMessage;
+
+public class DataUnicastMessage extends FcmUnicastMessage<Object> {
+
+    private final Object data;
+    private final NotificationPayload notification;
+
+    public DataUnicastMessage(FcmMessageOptions options, String to, Object data) {
+        this(options, to, data, null);
+    }
+
+    public DataUnicastMessage(FcmMessageOptions options, String to, Object data, NotificationPayload notification) {
+        super(options, to);
+
+        this.data = data;
+        this.notification = notification;
+    }
+
+    @Override
+    @JsonProperty("data")
+    public Object getPayload() {
+        return data;
+    }
+
+
+    @JsonProperty("notification")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public NotificationPayload getNotificationPayload() {
+        return this.notification;
+    }
+
+}
